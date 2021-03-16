8ab1de436977f00fa6597d8c676f251a5a4dcc78
lord <lord.nemo@protonmail.com>
2021-03-16 23:30:22 +0300

    fixed /203/7.cpp (i hope so)

--- 203/7.cpp
+++ 203/7.cpp
@@ -13,36 +13,37 @@ int main() //TODO test this shit
     */
 
     std::fstream file("input");
-    int32_t temp, q=4, cnt = 0, k = 0, m = INF; // k - the number of numbers; m - position to place the sum (after m-th number)
-    for (int32_t i = 0, tmp; !file.eof(); ++i){
-        ++k;
-        file.read(reinterpret_cast<char*>(&tmp), q);
-        std::cout << tmp << '\n';
+    file.seekg(0, std::ios::end);
+    int32_t cnt = 0;   // count of positives
+    size_t  m   = -1, // position of the first positive
+            k   = file.tellg() / sizeof(int32_t); // count of numbers
+    file.seekg(0, std::ios::beg);
+    for (size_t i = 0; i < k; ++i) {
+        int32_t tmp;
+        file.read(reinterpret_cast<char*>(&tmp), sizeof(tmp));
+        std::cerr << "Read " << tmp << '\n';
         if (tmp >= 0)
             ++cnt;
-        if (m == INF && tmp > 0)
+        if (m == -1 && tmp > 0)
             m = i;
     }
-    file.seekp(q * m, std::fstream::beg);
-    file.read(reinterpret_cast<char*>(&temp), q);
-    std::cout << cnt << ' ' << m << ' ' << k << std::endl; //DEBUG
+    std::cerr << "cnt = " << cnt << "\nm = " << m << '\n';
     file.close();
         
     file.open("input");
-    file.seekp(q * m, std::fstream::beg);
-    file.write(reinterpret_cast<char*>(&cnt), q);
-    file.seekp(q * (m + 1), std::fstream::beg);
-    for(int32_t i = m + 1, anotherTemp; i < k; ++i){
-        file.read(reinterpret_cast<char*>(&anotherTemp), q);
-        file.seekp(q * i, std::fstream::beg);
-        file.write(reinterpret_cast<char*>(&temp), q);
-        file.seekp(q * (i + 1), std::fstream::beg);
-        temp = anotherTemp;
-        std::cout << temp << std::endl;
+    file.seekp(sizeof(int32_t) * m, std::fstream::beg);
+    int32_t to_write = cnt;
+    for (size_t i = m; i < k; ++i) {
+        int32_t buffer = 0;
+        file.read(reinterpret_cast<char*>(&buffer), sizeof(buffer));
+        file.seekp(sizeof(to_write) * i, std::fstream::beg);
+        file.write(reinterpret_cast<char*>(&to_write), sizeof(to_write));
+        to_write = buffer;
     }
+    file.seekp(sizeof(to_write) * k, std::fstream::beg);
+    file.write(reinterpret_cast<char*>(&to_write), sizeof(to_write));
 
-    file.seekp(0, std::fstream::end);
-    file.write(reinterpret_cast<char*>(&temp), q);
     file.close();
 }
+
 
